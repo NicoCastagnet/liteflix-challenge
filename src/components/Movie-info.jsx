4d886d75a825e338fcc9e32545a8ce3744fb3c2b
Nicolas <nicolascastagnet002@gmail.com>
2023-03-21 01:36:12 -0300

    Movie info animated

--- src/components/Movie-info.jsx
+++ src/components/Movie-info.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import Play from './icons/Play';
 import Plus from './icons/Plus';
@@ -5,13 +6,25 @@ import Plus from './icons/Plus';
 const MovieInfo = ({ name }) => {
   return (
     <div className='flex flex-col items-left justify-end text-start w-full h-auto my-14 max-sm:text-center max-sm:pt-24'>
-      <h2 className='text-white uppercase font-light max-sm:text-2xl'>
+      <motion.h2
+        initial={{ translateY: -1500 }}
+        animate={{ translateY: 0 }}
+        transition={{ duration: 1.2, delay: 0.5 }}
+        className='text-white uppercase font-light max-sm:text-2xl'>
         Original de <span className='font-semibold'>liteflix</span>
-      </h2>
-      <h1 className='text-lf-aqua font-bold text-7xl uppercase max-sm:text-5xl max-sm:w-full'>
+      </motion.h2>
+      <motion.h1
+        initial={{ translateX: -1500 }}
+        animate={{ translateX: 0 }}
+        transition={{ duration: 1.2, delay: 0.5 }}
+        className='text-lf-aqua font-bold text-7xl uppercase max-sm:text-5xl max-sm:w-full'>
         {name}
-      </h1>
-      <div className='flex gap-5 mt-8 w-full max-sm:flex-col max-sm:items-center'>
+      </motion.h1>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, delay: 1.5 }}
+        className='flex gap-5 mt-8 w-full max-sm:flex-col max-sm:items-center'>
         <button className='flex items-center justify-center text-white fill-white gap-2 uppercase font-normal bg-lf-btn1 px-16 py-4 text-lg hover:bg-lf-btn1-hover transition-all max-sm:w-[80%]'>
           <Play className='w-6 h-6 fill-none stroke-white' />
           Reproducir
@@ -20,7 +33,7 @@ const MovieInfo = ({ name }) => {
           <Plus className='w-6 h-6' />
           Mi lista
         </button>
-      </div>
+      </motion.div>
     </div>
   );
 };
